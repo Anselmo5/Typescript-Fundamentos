90a95020ec82f99c08d506613e5316977c0355a0
Anselmo5 <anselmohenrique191@gmail.com>
2023-06-16 08:21:18 -0300

    Update Funções.ts

--- Fundamentos/Funções.ts
+++ Fundamentos/Funções.ts
@@ -1,5 +1,11 @@
-function somar(x: number, y:number | string): number{
-    return x+y ;
+function somar(x: number, y: number ): number { //Aqui temos um erro onde estamos passando como parametro valores com number, mas so que no codigo temos uma string, então temos que colocar na função uma condição que pegue duas condições formas em uma so
+    return x + y ;
 }
 
-console.log( );
+
+// tipos de entradas
+// um unico tipo de saida
+
+console.log( somar(5,6));
+console.log(somar(5,4));
+
